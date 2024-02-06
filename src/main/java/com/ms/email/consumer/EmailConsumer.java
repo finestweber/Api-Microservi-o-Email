ed1df9a516a9914ce3e76ddb49d6a85b045b63ec
Gamaliel Pereira <finestweber@gmail.com>
2024-02-06 13:33:52 -0300

    criando o consumer e a conexão

--- src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -2,7 +2,9 @@ package com.ms.email.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailConsumer {
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listEmailQueue(@Payload String string){
