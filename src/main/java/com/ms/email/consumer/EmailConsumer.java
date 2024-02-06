63a3ddce5fc7478c0912c23a94c0b5603e31935e
Gamaliel Pereira <finestweber@gmail.com>
2024-02-06 13:41:31 -0300

    V1 microserviço

--- src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.ms.email.consumer;
 
+import com.ms.email.dtos.EmailRecordDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumer {
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listEmailQueue(@Payload String string){
-        System.out.println(string);
+    public void listEmailQueue(@Payload EmailRecordDto emailRecordDto){
+        System.out.println(emailRecordDto.emailTo());
     }
 }
